82b751b2e77a9515733f600b98dd9af3ff58ccda
OTree <kevin10410@gmail.com>
2021-10-05 17:19:28 +0800

    feat(main): implement apolloProvider

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import '@/library/fontawesome';
 import '@/index.css';
+import apolloProvider from '@/graphql';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  apolloProvider,
   render: (h) => h(App),
 }).$mount('#app');
