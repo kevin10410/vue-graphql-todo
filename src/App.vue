a5567edb23df8e05f2b7fb7b4a74bf16d865e948
OTree <kevin10410@gmail.com>
2021-10-05 23:20:48 +0800

    feat(App): implement addTask

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app" class="todoapp stack-large">
     <h1>Vue GraphQL Todo</h1>
-    <Form/>
+    <Form
+      :name="name"
+      :error="addTaskError"
+      :loading="addTaskLoading"
+      @addTask="addTaskHandler"
+      @inputChanged="inputChangedHandler"
+    />
     <div class="filters btn-group stack-exception">
       <FilterButton
         v-for="filterName in filterNames"
@@ -23,10 +29,10 @@
 </template>
 
 <script>
-import gql from 'graphql-tag';
 import Form from '@/components/Form.vue';
 import TaskList from '@/components/TaskList/index.vue';
 import FilterButton from '@/components/FilterButton.vue';
+import { QUERY_TASK, ADD_TASK } from '@/graphql/query';
 
 const FILTER_MAP = {
   All: () => true,
@@ -42,16 +48,14 @@ export default {
       filter: 'All',
       queryTask: [],
       queryError: null,
+      addTaskError: null,
+      addTaskLoading: false,
     };
   },
   apollo: {
-    queryTask: gql`query {
-      queryTask {
-        id
-        title
-        completed
-      }
-    }`,
+    queryTask: {
+      query: QUERY_TASK,
+    },
   },
   computed: {
     filterNames() {
@@ -73,6 +77,32 @@ export default {
   methods: {
     filterUpdatedHandler(filter) {
       this.filter = filter;
+    },
+    inputChangedHandler(e) {
+      const { value } = e.target;
+      this.name = value;
+    },
+    addTaskHandler() {
+      this.addTaskError = null;
+      this.addTaskLoading = true;
+      this.$apollo.mutate({
+        mutation: ADD_TASK,
+        variables: {
+          title: this.name,
+        },
+        update: (cache, { data: { addTask } }) => {
+          const data = cache.readQuery({ query: QUERY_TASK });
+          const { task } = addTask;
+          data.queryTask.push(task[0]);
+          cache.writeQuery({ data, query: QUERY_TASK });
+        },
+      }).then(() => {
+        this.name = '';
+        this.addTaskLoading = false;
+      }).catch((error) => {
+        this.addTaskError = error;
+        this.addTaskLoading = false;
+      });
     },
   },
   components: {
